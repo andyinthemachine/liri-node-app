04b426d4ac0da46c4d08749c653efe9025cf8115
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-06-25 16:42:34 -0600

    inquire added

--- liri.js
+++ liri.js
@@ -5,20 +5,37 @@
 // Client ID bfb08deb54d546a7a4f7af2e0e894865
 // Client Secret 52b56af3e1e745fcbc5c9111d12449fe
 
-// var moment = require('moment');
-// moment().format();
 
 require("dotenv").config();
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
+var moment = require("moment");
 
+var inquirer = require("inquirer");
+
+
+// // moment().format();
 
 // concert-this
 // spotify-this-song
 // movie-this
 // do-what-it-says
+
+inquirer.prompt([
+    {
+        type: "list",
+        name: "mediachoice",
+        message: "What would you like to look up?",
+        choices: ["A concert", "A song", "A movie","Read from file"]
+    }
+]).then(function(response) {
+    console.log(`You chose ${response.mediachoice}`);
+});
+
+
 
 //          node liri.js concert-this <artist/band name here>
 // "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
@@ -80,7 +97,7 @@ var spotify = new Spotify(keys.spotify);
 // * Plot of the movie.
 // * Actors in the movie.
 
-
+/*
 axios.get("http://www.omdbapi.com/?t=jaws&y=&plot=short&apikey=trilogy").then(
     function (response) {
         console.log("The movie's rating is: " + response.data.imdbRating);
@@ -101,10 +118,32 @@ axios.get("http://www.omdbapi.com/?t=jaws&y=&plot=short&apikey=trilogy").then(
         }
         console.log(error.config);
     });
+*/
+
+/*
+fs.readFile("random.txt", "utf8", function (error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+        return console.log(error);
+    }
+
+    // We will then print the contents of data
+    console.log(data);
+
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+
+    // We will then re-display the content as an array for later use.
+    console.log(dataArr);
+
+});
+*/
+
 
 //          node liri.js do-what-it-says
 // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
 // Edit the text in random.txt to test out the feature for movie-this and concert-this.
 
-console.log(process.argv);
+// console.log(process.argv);
